ce17b1d377c72d8939f978132a5d0bb327a642e3
desmond60 <artyuxov169@gmail.com>
2023-03-10 11:50:43 +0700

    Corrected grid
    +  Add test parallel item

--- FEM/other/Generate.cs
+++ FEM/other/Generate.cs
@@ -1,4 +1,6 @@
-﻿namespace FEM;
+﻿using ScottPlot.Drawing.Colormaps;
+
+namespace FEM;
 
 
 //: Построение сетки
@@ -32,113 +34,167 @@ public partial class MainWindow : Window
         double copy_Y_min = Y_min;
         double copy_X_min = X_min;
 
-        border_X = border_X.OrderByDescending(n => n).ToList();
-        border_Y = border_Y.OrderByDescending(n => n).ToList();
+        border_X = border_X.OrderByDescending(n => n).Distinct().ToList();
+        border_Y = border_Y.OrderByDescending(n => n).Distinct().ToList();
+
+        // Сортировка по X
+        for (int write = 0; write < items.Count; write++)
+            for (int sort = 0; sort < items.Count - 1; sort++)
+                if (items[sort].Begin[0] > items[sort + 1].Begin[0]) {
+                    Item temp = items[sort + 1] with { };
+                    items[sort + 1] = items[sort] with { };
+                    items[sort] = temp with { };
+                }
+
+        // Список индексов объектов (нужен чтобы пропускать пройденные объекты)
+        List<int> index_list = new List<int>() { 0 };
 
         // Составляем листы шагов X
         List<double> H_Axe_X = new List<double>();
 
-        // Находим объект
-        int id = 0;
-        for (int i = 0; i < items.Count; i++) {
-            if (X_min == items[i].Begin[0]) {
-                id = i;
-                break;
-            }
-        }
-
         // Считаем шаг по объекту
-        double Hx = (items[id].End[0] - items[id].Begin[0]) / items[id].Nx; // Шаг по мал. полю Ось X
-        double X_temp_max = items[id].End[0];
+        double Hx = (items[0].End[0] - items[0].Begin[0]) / items[0].Nx; 
+        double X_temp_max = items[0].End[0];
 
         // Генерация шагов по оси X
         while (X_min < X_max) {
 
-            while (X_min < X_temp_max) {
+            while (X_min < X_temp_max && Abs(X_temp_max - X_min) > 1e-6) {
                 X_min += Hx;
-                H_Axe_X.Add(Hx); 
+                H_Axe_X.Add(Hx);
             }
+
+            X_min -= Hx;
+            H_Axe_X.RemoveAt(H_Axe_X.Count - 1);
+            var step = Abs(X_temp_max - X_min);
+            H_Axe_X.Add(step);
+            X_min += step;
 
             // Проверяем входит ли он в другой объект
             bool isFound = false;
             for (int i = 0; i < items.Count; i++) {
-                if (X_min > items[i].Begin[0] && X_min < items[i].End[0] && id != i) {
-                    id = i;
+                if (X_min > items[i].Begin[0] && X_min < items[i].End[0] && !index_list.Contains(i)) {
+                    index_list.Add(i);
                     isFound = true;
-                    Hx = (items[id].End[0] - items[id].Begin[0]) / items[id].Nx; // Шаг по мал. полю Ось X
-                    X_temp_max = items[id].End[0];
+                    Hx = (items[i].End[0] - items[i].Begin[0]) / items[i].Nx;
+                    X_temp_max = items[i].End[0];
                     break;
                 }
             }
 
-            if (X_min >= X_max)
+            // Если наступили на границу
+            if (Abs(X_min - X_max) <= 1e-6)
                 break;
+
+            // Если перескочили границу
+            if (X_min > X_max) {
+                X_min -= Hx;
+                H_Axe_X.RemoveAt(H_Axe_X.Count - 1);
+                H_Axe_X.Add(Abs(items[^1].End[0] - X_min));
+                break;
+            }
 
             while (!isFound) {
                 X_min += Hx;
                 H_Axe_X.Add(Hx);
 
                 for (int i = 0; i < items.Count; i++) {
-                    if (X_min > items[i].Begin[0] && X_min < items[i].End[0]) {
-                        id = i;
+                    if (X_min > items[i].Begin[0] && X_min < items[i].End[0] && !index_list.Contains(i)) {
+
+                        X_min -= Hx;
+                        H_Axe_X.RemoveAt(H_Axe_X.Count - 1);
+                        step = Abs(items[i].Begin[0] - X_min);
+                        if (step > min_step)
+                            H_Axe_X.Add(step);
+                        X_min += step;
+
+                        index_list.Add(i);
                         isFound = true;
-                        Hx = (items[id].End[0] - items[id].Begin[0]) / items[id].Nx; // Шаг по мал. полю Ось X
-                        X_temp_max = items[id].End[0];
+                        Hx = (items[i].End[0] - items[i].Begin[0]) / items[i].Nx;
+                        X_temp_max = items[i].End[0];
                         break;
                     }
                 }
             }
         }
 
+
+        // Сортировка по Y
+        for (int write = 0; write < items.Count; write++)
+            for (int sort = 0; sort < items.Count - 1; sort++)
+                if (items[sort].Begin[1] > items[sort + 1].Begin[1]) {
+                    Item temp = items[sort + 1] with { };
+                    items[sort + 1] = items[sort] with { };
+                    items[sort] = temp with { };
+                }
+
         // Составляем листы шагов Y
         List<double> H_Axe_Y = new List<double>();
 
         // Находим объект
-        id = 0;
-        for (int i = 0; i < items.Count; i++){
-            if (Y_min == items[i].Begin[1]){
-                id = i;
-                break;
-            }
-        }
+        index_list.Clear();
+        index_list.Add(0);
 
         // Считаем шаг по объекту
-        double Hy = (items[id].End[1] - items[id].Begin[1]) / items[id].Ny; // Шаг по мал. полю Ось Y
-        double Y_temp_max = items[id].End[1];
+        double Hy = (items[0].End[1] - items[0].Begin[1]) / items[0].Ny;
+        double Y_temp_max = items[0].End[1];
 
-        // Генерация шагов по оси X
+        // Генерация шагов по оси Y
         while (Y_min < Y_max) {
 
-            while (Y_min < Y_temp_max) {
+            while (Y_min < Y_temp_max && Abs(Y_temp_max - Y_min) > 1e-6) {
                 Y_min += Hy;
                 H_Axe_Y.Add(Hy);
             }
 
+            Y_min -= Hy;
+            H_Axe_Y.RemoveAt(H_Axe_Y.Count - 1);
+            var step = Abs(Y_temp_max - Y_min);
+            H_Axe_Y.Add(step);
+            Y_min += step;
+
             // Проверяем входит ли он в другой объект
             bool isFound = false;
             for (int i = 0; i < items.Count; i++) {
-                if (Y_min > items[i].Begin[1] && Y_min < items[i].End[1] && id != i) {
-                    id = i;
+                if (Y_min > items[i].Begin[1] && Y_min < items[i].End[1] && !index_list.Contains(i)) {
+                    index_list.Add(i);
                     isFound = true;
-                    Hy = (items[id].End[1] - items[id].Begin[1]) / items[id].Ny; // Шаг по мал. полю Ось Y
-                    Y_temp_max = items[id].End[1];
+                    Hy = (items[i].End[1] - items[i].Begin[1]) / items[i].Ny;
+                    Y_temp_max = items[i].End[1];
                     break;
                 }
             }
 
-            if (Y_min >= Y_max)
+            // Если наступили на границу
+            if (Abs(Y_min - Y_max) <= 1e-6)
                 break;
+
+            // Если перескочили границу
+            if (Y_min > Y_max) {
+                Y_min -= Hy;
+                H_Axe_Y.RemoveAt(H_Axe_Y.Count - 1);
+                H_Axe_Y.Add(Abs(items[^1].End[1] - Y_min));
+                break;
+            }
 
             while (!isFound) {
                 Y_min += Hy;
                 H_Axe_Y.Add(Hy);
 
                 for (int i = 0; i < items.Count; i++) {
-                    if (Y_min > items[i].Begin[1] && Y_min < items[i].End[1]) {
-                        id = i;
+                    if (Y_min > items[i].Begin[1] && Y_min < items[i].End[1] && !index_list.Contains(i)) {
+
+                        Y_min -= Hy;
+                        H_Axe_Y.RemoveAt(H_Axe_Y.Count - 1);
+                        step = Abs(items[i].Begin[1] - Y_min);
+                        if (step > min_step)
+                            H_Axe_Y.Add(step);
+                        Y_min += step;
+
+                        index_list.Add(i);
                         isFound = true;
-                        Hy = (items[id].End[1] - items[id].Begin[1]) / items[id].Ny; // Шаг по мал. полю Ось X
-                        Y_temp_max = items[id].End[1];
+                        Hy = (items[i].End[1] - items[i].Begin[1]) / items[i].Ny;
+                        Y_temp_max = items[i].End[1];
                         break;
                     }
                 }
@@ -148,8 +204,8 @@ public partial class MainWindow : Window
         // Учет границ Y
         H_Axe_Y.Insert(0, 0);
         double Y_temp = copy_Y_min;
-        id = 1;
-        List<(int index, double val_layer, double val_Y)> new_Axe = new List<(int, double, double)>();
+        int id = 1;
+        List<(int index, double val_layer, double val)> new_Axe = new List<(int, double, double)>();
         for (int i = 0; i < H_Axe_Y.Count && id != border_Y.Count + 1; i++) {
 
             // Прибавляем шаг
@@ -170,7 +226,7 @@ public partial class MainWindow : Window
 
         // Корректировка шагов по Оси Y
         for (int i = 0; i < new_Axe.Count; i++) {
-            double step = Abs(new_Axe[i].val_Y - new_Axe[i].val_layer);
+            double step = Abs(new_Axe[i].val_layer - new_Axe[i].val);
             H_Axe_Y.Insert(new_Axe[i].index, step);
             H_Axe_Y[new_Axe[i].index + 1] = Abs(H_Axe_Y[new_Axe[i].index + 1] - step);
         }
@@ -178,17 +234,18 @@ public partial class MainWindow : Window
         // Корректировка шагов, чтобы не было узких прямоугольников
         List<int> index_rem = new List<int>();
         for (int i = 1; i < H_Axe_Y.Count - 1; i++) {
-            if (H_Axe_Y[i] < min_step) {
+            if (H_Axe_Y[i] <= min_step) {
                 H_Axe_Y[i - 1] += H_Axe_Y[i];
                 index_rem.Add(i);
             }
         }
 
         // Удлаение не подходящих шагов
-        for (int i = 0; i < index_rem.Count; i++)
+        for (int i = 0; i < index_rem.Count; i++) {
             H_Axe_Y.RemoveAt(index_rem[i]);
+            index_rem = index_rem.Select(n => n - 1).ToList();
+        }
         H_Axe_Y.RemoveAt(0);
-
 
         // Учет границ X
         H_Axe_X.Insert(0, 0);
@@ -215,7 +272,7 @@ public partial class MainWindow : Window
 
         // Корректировка шагов по Оси Y
         for (int i = 0; i < new_Axe.Count; i++) {
-            double step = Abs(new_Axe[i].val_Y - new_Axe[i].val_layer);
+            double step = Abs(new_Axe[i].val_layer - new_Axe[i].val);
             H_Axe_X.Insert(new_Axe[i].index, step);
             H_Axe_X[new_Axe[i].index + 1] = Abs(H_Axe_X[new_Axe[i].index + 1] - step);
         }
@@ -230,14 +287,15 @@ public partial class MainWindow : Window
         }
 
         // Удлаение не подходящих шагов
-        for (int i = 0; i < index_rem.Count; i++)
+        for (int i = 0; i < index_rem.Count; i++) {
             H_Axe_X.RemoveAt(index_rem[i]);
+            index_rem = index_rem.Select(n => n - 1).ToList();
+        }
         H_Axe_X.RemoveAt(0);
-
 
         // Генерация шагов по оси X от объекта
         double temp_V = copy_X_min;
-        double temp_H = Hx;
+        double temp_H = H_Axe_X[0];
         while (temp_V >= Begin_BG[0]) {
             temp_H *= Kx;
             H_Axe_X.Insert(0, temp_H);
@@ -250,12 +308,12 @@ public partial class MainWindow : Window
         double X = IsStrictGrid ? Begin_BG[1] : temp_V;
 
         temp_V = X_max;
-        temp_H = Hx;
+        temp_H = H_Axe_X[^1];
         while (temp_V <= End_BG[0]) {
             temp_H *= Kx;
             H_Axe_X.Add(temp_H);
             temp_V += temp_H;
-            if (temp_V >= End_BG[0] && IsStrictGrid) {
+            if (temp_V > End_BG[0] && IsStrictGrid) {
                 temp_H -= (temp_V - End_BG[0]);
                 H_Axe_X[^1] = temp_H;
             }
@@ -263,7 +321,7 @@ public partial class MainWindow : Window
 
         // Генерация шагов по оси Y от объекта
         temp_V = copy_Y_min;
-        temp_H = Hy;
+        temp_H = H_Axe_Y[0];
         while (temp_V >= Begin_BG[1]) {
             temp_H *= Ky;
             H_Axe_Y.Insert(0, temp_H);
@@ -276,12 +334,12 @@ public partial class MainWindow : Window
         double Y = IsStrictGrid ? Begin_BG[0] : temp_V;
 
         temp_V = Y_max;
-        temp_H = Hy;
+        temp_H = H_Axe_Y[^1];
         while (temp_V <= End_BG[1]) {
             temp_H *= Ky;
             H_Axe_Y.Add(temp_H);
             temp_V += temp_H;
-            if (temp_V >= End_BG[1] && IsStrictGrid) {
+            if (temp_V > End_BG[1] && IsStrictGrid) {
                 temp_H -= (temp_V - End_BG[1]);
                 H_Axe_Y[^1] = temp_H;
             }
@@ -315,7 +373,7 @@ public partial class MainWindow : Window
 
             // Корректировка шагов по Оси Y
             for (int i = 0; i < new_Axe.Count; i++) {
-                double step = Abs(new_Axe[i].val_Y - new_Axe[i].val_layer);
+                double step = Abs(new_Axe[i].val_layer - new_Axe[i].val);
                 H_Axe_Y.Insert(new_Axe[i].index, step);
                 H_Axe_Y[new_Axe[i].index + 1] = Abs(H_Axe_Y[new_Axe[i].index + 1] - step);
             }
@@ -329,9 +387,11 @@ public partial class MainWindow : Window
                 index_rem.Add(i);
             }
         }
+
         // Удлаение не подходящих шагов
         for (int i = 0; i < index_rem.Count; i++) {
             H_Axe_Y.RemoveAt(index_rem[i]);
+            index_rem = index_rem.Select(n => n - 1).ToList();
         }
 
         // Сколько будет линий на графике (Количество узлов на Осях)
